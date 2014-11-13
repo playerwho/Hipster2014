462e357627dca8f045ac1aa2645c3822d8ee764b
Austin Widmeier <austinwidmeier@gmail.com>
2014-11-13 13:41:20 -0700

    dsfvedsvvv
    vwesvsdvdsvds

--- src/ctec/hipster/controller/HipsterAppController.java
+++ src/ctec/hipster/controller/HipsterAppController.java
@@ -1,5 +1,6 @@
 package ctec.hipster.controller;
 
+import ctec.hipster.model.Hipster;
 import ctec.hipster.view.HipsterFrame;
 /**
  *  HipsterAppController for the 2014 Hipster day project
@@ -9,14 +10,34 @@ import ctec.hipster.view.HipsterFrame;
 public class HipsterAppController
 {
 	private HipsterFrame baseFrame;
+	private Hipster selfHipster;
+	private String [] myAlbums;
 	
 	public HipsterAppController()
 	{
+		selfHipster = new Hipster("John", 69);
+		buildAlbumArray();
 		baseFrame = new HipsterFrame(this);
 	}
 	
 	public void start()
 	{
+		selfHipster.setHipsterAlbums(myAlbums);
+	}
+	
+	private void buildAlbumArray()
+	{
+		myAlbums = new String[4];
+		myAlbums[0] = "Hello Nasty";
+		myAlbums[1] = "Modest Mouse";
+		myAlbums[2] = "fhfhdhd";
+		myAlbums[3] = "fsdapo wowbfw";
 		
+		// hipster
+		String [] otherAlbums = {"stuff", "other", "sop sign", "hipster array"};
+		
+		// ultrahipster
+		String [] weirdAlbums;
+		weirdAlbums = new String [] {"ewefc", "aasas", "dsda", "dsd"};
 	}
 }
--- src/ctec/hipster/view/HipsterPanel.java
+++ src/ctec/hipster/view/HipsterPanel.java
@@ -19,8 +19,10 @@ public class HipsterPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		albumLabel = new JLabel("album info here");
-		hipsterImage= new JLabel("name");
+		hipsterImage = new JLabel("something cool lookin", new ImageIcon(HipsterPanel.class.getResource("/ctec/hipster/view/images/cam.jpg")), JLabel.CENTER);
+		hipsterImage.setVerticalAlignment(SwingConstants.BOTTOM);
 		albumBox = new JComboBox();
+		
 		
 		
 		setupComboBox();
@@ -48,10 +50,10 @@ public class HipsterPanel extends JPanel
 	{
 		baseLayout.putConstraint(SpringLayout.WEST, hipsterImage, 237, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, hipsterImage, -88, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, albumLabel, 3, SpringLayout.NORTH, albumBox);
-		baseLayout.putConstraint(SpringLayout.WEST, albumLabel, 256, SpringLayout.EAST, albumBox);
 		baseLayout.putConstraint(SpringLayout.NORTH, albumBox, 99, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, albumBox, 92, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, albumLabel, 3, SpringLayout.NORTH, albumBox);
+		baseLayout.putConstraint(SpringLayout.WEST, albumLabel, 340, SpringLayout.EAST, albumBox);
 	}
 	
 	private void setupListeners()
